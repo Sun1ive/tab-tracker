15cd3c43e5593968d1443ff2f705aafcf5a73b99
Sun1ive <sunliveua@gmail.com>
2018-04-05 14:31:17 +0300

    zzz

--- server/src/policies/AuthenticationControllerPolicy.js
+++ server/src/policies/AuthenticationControllerPolicy.js
@@ -3,9 +3,25 @@ import Joi from 'joi';
 module.exports = {
   register(req, res, next) {
     const schema = {
-      email: Joi.stirng().email(),
+      email: Joi.string().email(),
       password: Joi.string().regex(new RegExp('^[a-zA-Z0-9]{6,32}$')),
     };
-    next();
+    const { error, value } = Joi.validate(req.body, schema);
+    if (error) {
+      switch (error.details[0].context.key) {
+        case 'email':
+          res.status(400).json({ error: 'Invalid email address' });
+          break;
+        case 'password':
+          res
+            .status(400)
+            .json({ error: 'Invalid password, should be between 6 and 32 characters' });
+          break;
+        default:
+          res.status(400).json({ error: 'Invalid registration information' });
+      }
+    } else {
+      next();
+    }
   },
 };
